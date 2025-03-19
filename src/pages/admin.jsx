ba5fee1ee6b18d0bfd5985dbec515815dfe26d42
SagathiyaJaydeep <sagathiyajaydeep66@gmail.com>
2025-03-19 09:37:17 +0530

    fetch category,sub-category and question count and set in dashboard page..

--- src/pages/admin.jsx
+++ src/pages/admin.jsx
@@ -1,33 +1,67 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
 import { Box, Container, Typography, Grid } from "@mui/material";
-
-const dashboardData = [
-  { title: "Total Categories", value: 3 },
-  { title: "Total Products", value: 15 },
-  { title: "Total Users", value: 120 },
-];
-
-const StatBox = ({ title, value }) => (
-  <Box
-    sx={{
-      boxShadow: "0px 0px 5px #ccc",
-      padding: "20px",
-      borderRadius: "5px",
-      textAlign: "center",
-      backgroundColor: "#fff",
-    }}
-  >
-    <Typography variant="h5" sx={{ marginBottom: "20px", fontWeight: 700 }}>
-      {title}
-    </Typography>
-    <Typography variant="h3" sx={{ fontWeight: 700 }}>
-      {value}
-    </Typography>
-  </Box>
-);
+import axios from "axios";
 
 function Admin() {
+  const [categoryData, setCategoryData] = useState(0);
+  const [subCategoryData, setSubCategoryData] = useState(0);
+  const [questionData, setQuestionData] = useState(0);
+  const Token =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY3ODA4YTE3MzRkZGY2ZjZlZGUyNTRmMSIsImlhdCI6MTc0MjE4MzU3NX0.Xwtx7dNyxspgDzx_WCS5nhRr8D46VrS0mkSfd-4aXFE";
+  const fetchCategoryData = async () => {
+    try {
+      await axios
+        .get("https://interviewback-ucb4.onrender.com/category/count", {
+          headers: {
+            Authorization: Token,
+          },
+        })
+        .then((res) => {
+          setCategoryData(res.data.data);
+        });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const fetchQuestionData = async () => {
+    try {
+      await axios
+        .get("https://interviewback-ucb4.onrender.com/subcategory/count", {
+          headers: {
+            Authorization: Token,
+          },
+        })
+        .then((res) => {
+          setSubCategoryData(res.data.data);
+        });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const fetchSubCategoryData = async () => {
+    try {
+      await axios
+        .get("https://interviewback-ucb4.onrender.com/questions/count", {
+          headers: {
+            Authorization: Token,
+          },
+        })
+        .then((res) => {
+          setQuestionData(res.data.data);
+        });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchCategoryData();
+    fetchSubCategoryData();
+    fetchQuestionData();
+  }, []);
   return (
     <>
       <Box sx={{ position: "relative" }}>
@@ -42,11 +76,69 @@ function Admin() {
         >
           <Container maxWidth="lg">
             <Grid container spacing={3}>
-              {dashboardData.map((item, index) => (
-                <Grid item xs={12} sm={6} md={4} key={index}>
-                  <StatBox title={item.title} value={item.value} />
-                </Grid>
-              ))}
+              <Grid item xs={12} sm={6} md={4}>
+                <Box
+                  sx={{
+                    boxShadow: "0px 0px 5px #ccc",
+                    padding: "20px",
+                    borderRadius: "5px",
+                    textAlign: "center",
+                    backgroundColor: "#fff",
+                  }}
+                >
+                  <Typography
+                    variant="h5"
+                    sx={{ marginBottom: "20px", fontWeight: 700 }}
+                  >
+                    Total Category
+                  </Typography>
+                  <Typography variant="h3" sx={{ fontWeight: 700 }}>
+                    {categoryData}
+                  </Typography>
+                </Box>
+
+                <Box
+                  sx={{
+                    boxShadow: "0px 0px 5px #ccc",
+                    padding: "20px",
+                    borderRadius: "5px",
+                    textAlign: "center",
+                    backgroundColor: "#fff",
+                  }}
+                >
+                  <Typography
+                    variant="h5"
+                    sx={{ marginBottom: "20px", fontWeight: 700 }}
+                  >
+                    Total Category
+                  </Typography>
+
+                  <Typography variant="h3" sx={{ fontWeight: 700 }}>
+                    {subCategoryData}
+                  </Typography>
+                </Box>
+
+                <Box
+                  sx={{
+                    boxShadow: "0px 0px 5px #ccc",
+                    padding: "20px",
+                    borderRadius: "5px",
+                    textAlign: "center",
+                    backgroundColor: "#fff",
+                  }}
+                >
+                  <Typography
+                    variant="h5"
+                    sx={{ marginBottom: "20px", fontWeight: 700 }}
+                  >
+                    Total Category
+                  </Typography>
+
+                  <Typography variant="h3" sx={{ fontWeight: 700 }}>
+                    {questionData}
+                  </Typography>
+                </Box>
+              </Grid>
             </Grid>
           </Container>
         </Box>
